a9e6dfa8d16f350bd3f9eddd776b62951b2150e7
Programing-School <mohamedehabam99@hotmail.com>
2022-02-18 14:42:43 +0200

    Deploy

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -35,7 +35,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: true,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
